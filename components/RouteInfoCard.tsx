7bf8e6597bd0dd6eaa55661967eeada4a3d7c593
Zaldy Jr Pagaduan <zaldypagaduanjr@gmail.com>
2024-11-05 14:11:57 +0800

    added UI updates

--- components/RouteInfoCard.tsx
+++ components/RouteInfoCard.tsx
@@ -26,17 +26,21 @@ const RouteInfoCard: React.FC<CardProps> = ({ routes, onButtonClick }) => {
   };
 
   return (
-    <div className="absolute top-4 right-4 bg-gray-800 text-white p-4 shadow-lg rounded-lg p-4 max-w-md">
-      <h2 className="text-lg font-semibold">Route Information</h2>
-      <Dropdown
-        label={selectedOption.label || ""}
-        options={currentRoutes}
-        onSelect={handleSelect}
-      />
-      <div className="mt-2 flex flex-col items-start">
+    <div className="absolute top-4 right-4 bg-gray-800 text-white p-4 shadow-lg rounded-lg max-w-md ">
+      <div className="sticky top-0">
+        <h2 className="text-lg font-semibold">Route Information</h2>
+        <Dropdown
+          label={selectedOption.label || ""}
+          options={currentRoutes}
+          onSelect={handleSelect}
+        />
+      </div>
+      <div className="mt-2 flex flex-col items-start max-h-64 overflow-y-auto">
         <p className="text-sm mb-2">{selectedOption.info}</p>
+      </div>
+      <div className="sticky bottom-0 w-full">
         <button
-          className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+          className="w-full px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
           onClick={() => onButtonClick(selectedOption)}
         >
           {selectedOption.isFollowed ? "Stop Following Route" : "Follow Route"}
